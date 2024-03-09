ca1c5f501a0d266254e0ce2adb617950773ebb5a
vkhydras <victorkimaru8@gmail.com>
2024-03-09 15:55:05 +0300

    future

--- dart/test.dart
+++ dart/test.dart
@@ -5,13 +5,127 @@
 // */
 
 // void test(List<String>? names) {
- 
+
 //  final length = names?.length ?? 0;
 //   print(length);
 // }
 
+//Enumirations
+
+// enum AnimalType{
+//   cat, dog, bunny
+// }
+
+// void test(AnimalType animalType){
+//   // print(animalType);
+//   switch(animalType){
+//     case AnimalType.bunny:
+//       print('Bunny');
+//       break;
+//     case AnimalType.cat:
+//       print('CAt');
+//       break;
+//     case AnimalType.dog:
+//       print('DOg');
+//       break;
+//   }
+// }
+
+//method of a class is a function inside the class
+// class Person {
+//   final String name;
+
+//   Person(this.name);
+
+//   void printName(){//instant function
+//     print(name);
+//   }
+// }
+// objects are instances of classes explnantion >>> class is like a blueprint/plan obejects are similar projects created from the blurprint/plan
+
+//absract class >> a class that cannot not be instatiated >> cannot have instanstses
+// abstract class LivingThing {
+//   void breathe(){
+//     print('Is breathing');
+//   }
+
+//   void move(){
+//     print('Moving');
+//   }
+// }
+
+// class Cat {
+//   final String name;
+
+//   Cat(this.name);
+// //>>>>factory constructor >> return an instance of the class
+//   // factory Cat.buz(){
+//   //   return Cat('buzz');
+//   // }
+
+//   // @override 
+//   // bool operator == (covariant Cat other) => other.name == name;
+
+//   // @override
+//   // int get hashCode => name.hashCode;
+// }
+
+// class Person {
+//   final String firstName;
+//   final String lastName;
+
+//   Person( 
+//     this.firstName,
+//     this.lastName
+//     );
+// }
+
+// extension FullNname on Person{
+//   String get fullName => '$firstName $lastName';
+// }
+
+// void test() {
+// //  final person = Person('foo');//object/instance of class
+// //  person.printName();
+
+//   // final buzz = Cat.buz();
+//   // print(buzz.name);
+
+//   // final cat1 = Cat('foo');
+//   // final cat2 = Cat('foo');
+
+//   // if(cat1 == cat2){
+//   //   print('They are equal');
+//   // } else {
+//   //   print('They are not equal');
+//   // }
+
+//   // final meow = Cat("Buzz");
+//   // print(meow.name);
+//   // meow.run();
+//   final foo = Person('foo', 'Bar');
+//   print(foo.fullName);
 
 
-void main (){
- 
+// }
+
+
+
+// extension Run on Cat {
+//   void run(){
+//     print('Cat $name is runnog');
+//   }
+// }
+
+Future<int> heavyFuture(int a){
+  return Future.delayed(Duration(seconds: 3),() => a*2);
+}
+
+void test() async {
+  final result  = await heavyFuture(20);
+  print(result);
+}
+
+void main() {
+  test();
 }
