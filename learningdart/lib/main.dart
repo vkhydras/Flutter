a39f53840416c402b39c2ee3e21ad16f8d5e07dc
vkhydras <victorkimaru8@gmail.com>
2024-03-09 11:11:39 +0300

    test

--- learningdart/lib/main.dart
+++ learningdart/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-  
+
 }
 
 class MyApp extends StatelessWidget {
@@ -73,7 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
-    //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
